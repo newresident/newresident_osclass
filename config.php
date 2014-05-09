de98e659356f07d6ee73a7f36abe79609c961b7f
newresident <referral@newresident.me>
2014-05-09 17:53:37 +0530

    Final
    Final Files

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * The base MySQL settings of Osclass
+ */
+define('MULTISITE', 0);
+
+/** MySQL database name for Osclass */
+define('DB_NAME', 'newresident');
+
+/** MySQL database username */
+define('DB_USER', 'resident');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'newrt@Golive111');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Table prefix */
+define('DB_TABLE_PREFIX', 'oc_');
+
+define('REL_WEB_URL', '/osclass/');
+
+define('WEB_PATH', 'http://newresident.me/');
+
+?>
